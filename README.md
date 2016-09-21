fe4528cb34c4a6e8a9063a187cb7e6337c09cec9
jiasir <jiasir@icloud.com>
2016-09-21 17:59:25 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # netprof
+
 A network profiler tool
+
+## Usage
+
+Get the latency from local to google.com in formated
+
+```Shell
+> .\netprof.exe -ping google.com
+2016-09-21 17:28:46,32,54,49
+2016-09-21 17:28:47,32,54,49
+2016-09-21 17:28:48,32,54,49
+```
+
+Get the local local network transmission in formated
+
+```Shell
+> .\netprof.exe -transmission
+2016-09-21 17:30:01,4021683948,795482278
+2016-09-21 17:30:02,4021683948,795482278
+2016-09-21 17:30:03,4021683948,795482278
+```
---
+++ main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os/exec"
+	"flag"
+	"log"
+	"os"
+	"strings"
+	"time"
+)
+
+func getTransmissionData(outPut bytes.Buffer) {
+	var ll []string
+	nl := 0
+	for {
+		nl++
+		line, err := outPut.ReadString('\n')
+		if err != nil {
+			break
+		}
+		ll = strings.Fields(line)
+		if nl == 5 {
+			fmt.Printf("%v,%v,%v\n", time.Now().Format("2006-01-02 15:04:05"), ll[1], ll[2])
+		}
+	}
+}
+
+func getPingData(outPut bytes.Buffer) {
+	var ll []string
+	nl :=0
+	for {
+		nl++
+		line, err := outPut.ReadString('\n')
+		if err != nil {
+			break
+		}
+		ll = strings.Fields(line)
+		if nl == 3 {
+			bytes := strings.Split(ll[3:][0], "=")[1]
+			timeInMS := strings.Split(ll[3:][1], "=")[1]
+			responseTime := strings.Split(timeInMS, "ms")[0]
+			ttl := strings.Split(ll[3:][2], "=")[1]
+			fmt.Printf("%v,%v,%v,%v\n", time.Now().Format("2006-01-02 15:04:05"), bytes, responseTime, ttl)
+		}
+	}
+}
+
+func main() {
+	transmission := flag.Bool("transmission", false, "Every second printing local network transmission, format: timestamp,received,sent")
+	ping := flag.String("ping", "", "Every second printing the latency from remote address, format: timestamp,bytes,time,ttl")
+	flag.Parse()
+
+	if len(os.Args) == 1 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if *transmission {
+		cmd := exec.Command("netstat", "-e")
+		var out bytes.Buffer
+		cmd.Stdout = &out
+		err := cmd.Run()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for {
+			getTransmissionData(out)
+			time.Sleep(time.Second * 1)
+		}
+	}
+
+	if *ping != "" {
+		cmd := exec.Command("ping", "-n", "1", *ping)
+		var out bytes.Buffer
+		cmd.Stdout = & out
+		err := cmd.Run()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for {
+			getPingData(out)
+			time.Sleep(time.Second * 1)
+		}
+	}
+
+}
